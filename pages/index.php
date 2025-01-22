b1939501a5bc16eeb452f419a0ed08b43179ed04
Forgetmenot46 <wwe543216@hotmail.com>
2025-01-22 20:27:47 +0700

    add a Jumbotron testing

--- pages/index.php
+++ pages/index.php
@@ -12,6 +12,21 @@ include '../includes/db_config.php';
 <body>
 
     <p>สวัสดีประเทศอเมริกา1123</p>
-    
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap 5 JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Jumbotron -->
+    <div class="container py-4">
+        <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Welcome!</h1>
+                <p class="col-md-8 fs-4">This is a simple jumbotron-style component using Bootstrap 5. You can use this space to showcase important content or announcements.</p>
+                <button class="btn btn-primary btn-lg" type="button">Learn more</button>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
